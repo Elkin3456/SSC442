3dff056de0a08d481f8061acadb108dfacd3ba50
jbeyrer <59703958+jbeyrer@users.noreply.github.com>
2020-04-29 09:31:01 -0400

    Add files via upload

---
+++ SSC_Final_Project_graphs.R
@@ -0,0 +1,19 @@
+library(ggplot2)
+library(dplyr)
+mast <- read.csv('https://raw.githubusercontent.com/Elkin3456/SSC442/master/mlb20yr_abs.csv')
+
+al_teams <- mast %>% 
+  filter(team %in% c("ANA", "BAL", "BOS", "CHA", "CLE", "DET", "HOU", "KCA", "MIN", "NYA", "OAK", "SEA",
+                     "TBA", "TEX", "TOR"))
+
+al_teams %>%
+  ggplot( aes(x=year_x, y=hits_t, group=team, color=team)) +
+  geom_line() + labs(title = "AL Team's Hits Over Time", x= "Year", y="Hits")
+
+al_teams %>%
+  ggplot( aes(x=year_x, y=time, group=team, color=team)) +
+  geom_line() + labs(title = "AL Team's Game Length Over Time", x="Year", y="Game Length(minutes)")
+
+al_teams %>%
+  ggplot( aes(x=year_x, y=att, group=team, color=team)) +
+  geom_line() + labs(title = "AL team's Attendance Over Time", x="Year", y="Attendance")
